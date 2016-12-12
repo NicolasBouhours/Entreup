b8e0a1d855682d3cfd997208e12a7891dfe453a2
NicolasBouhours <nico53960@hotmail.fr>
2016-12-12 22:55:17 +0100

    Create Home Page

--- assets/app/core/home/home.component.ts
+++ assets/app/core/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+import { Component, OnInit, trigger, state, style, transition, animate, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
+    activity = "running";
+    animState = "normal";
+    sportChangeSubscrition: Subscription;
 
+    ngOnInit() {
+        let timer = Observable.timer(3000,3000);
+        let sportChangeSubscrition = timer.subscribe(t=> {
+            this.onChangeActivity();
+        });
+    }
+
+    onChangeActivity() {
+        this.animState ='normal';
+        if (this.activity === "running") {
+            this.activity = "vélo";
+            return 0;
+        } if (this.activity === "vélo") {
+            this.activity = "tennis";
+            return 0;
+        }
+        if (this.activity === "tennis") {
+            this.activity = "running";
+            return 0;
+        }
+    }
+
+    ngOnDestroy() {
+        this.sportChangeSubscrition.unsubscribe();
+    }
 }
